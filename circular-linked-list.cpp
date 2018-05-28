fa22ea6d805d42196a89e4e1fa772df26127cd5c
Thiago Silva <thiago.silva@iMac-de-suporte.local>
2018-05-28 19:40:56 -0300

    Finished push front function

--- circular-linked-list.cpp
+++ circular-linked-list.cpp
@@ -16,4 +16,17 @@ void CircularLinkedList<CLL>::push_front(CLL content)
 {
   struct node<CLL> *newNode = new struct node<CLL>;
   newNode->content = content;
+
+  if (empty())
+  {
+    tail->next = newNode;
+    newNode->next = newNode;
+  }
+  else
+  {
+    newNode->next = newNode;
+    tail->next->next = newNode;
+  }
+  head->next = newNode;
+  length++;
 }
