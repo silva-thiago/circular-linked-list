1bee237b0211a35c54325c41eee3fe5f04d089dd
Thiago Silva <thiago.silva@imd.ufrn.br>
2018-05-28 23:14:07 -0300

    Added elements to test the algorithm

--- main.cpp
+++ main.cpp
@@ -2,6 +2,21 @@
 
 int main(int argc, char const *argv[])
 {
-  /* code */
+  CircularLinkeList<int> list;
+
+  list.PushBack(150);
+  list.PushBack(160);
+  list.PushBack(170);
+  list.PushBack(180);
+  list.PushBack(190);
+  
+  list.PushFront(100);
+  list.PushFront(110);
+  list.PushFront(120);
+  list.PushFront(130);
+  list.PushFront(140);
+
+  list.Print();
+
   return 0;
 }
